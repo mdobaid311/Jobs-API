8eb325c4ee4e9ccd3581656ddb8827411dda2319
Mohammed Obaid <nameerasultana4747@gmail.com>
2022-07-25 19:35:26 +0530

    initial commit

---
+++ models/Job.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 50,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      required: [true, "Please provide a user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobSchema);
